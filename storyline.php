87f5dfe2b425b0cc27bc6d255323f7bd1a027f91
Kuangyou (Key) Yao <kuangyou@cs.washington.edu>
2016-06-16 14:58:06 -0700

    TimeLine skeleton added, PHP server script included

--- storyline.php
+++ storyline.php
@@ -1,4 +1,8 @@
 <?php
+    ini_set('display_errors', 1);
+    ini_set('display_startup_errors', 1);
+    error_reporting(E_ALL);
+
 	$db = new PDO("mysql:host=keycynthia-timeline.cupo2yigmrng.us-east-1.rds.amazonaws.com;dbname=keycynthia;charset=utf8mb4", "kuangyou", "461298cowboy");
 	$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
