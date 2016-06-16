8ea7f1ba47925690b3343b78ab4c243fef0c7296
Kuangyou (Key) Yao <kuangyou@cs.washington.edu>
2016-06-16 14:26:31 -0700

    TimeLine skeleton added, PHP server script included

--- js/gallery.js
+++ js/gallery.js
@@ -5,10 +5,10 @@ var imageBucketName = "keycynthia-image";
 
 AWS.config.update({accessKeyId: 'AKIAID52G7HATYHNRHCA', secretAccessKey: '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'});
 AWS.config.region = 'us-west-2';
-var s3Client = new AWS.S3();
+var s3 = new AWS.S3();
 
-function getS3Images() {
-    s3Client.listObjects({Bucket: imageBucketName}, function(error, data) {
+function loadImages() {
+    s3.listObjects({Bucket: imageBucketName}, function(error, data) {
         if (error) {
             alert(error); // an error occurred
         } else {
@@ -19,14 +19,14 @@ function getS3Images() {
 
             for (var i = 0; i < data.Contents.length; i++) {
                 var currImageName = data.Contents[i].Key;
-                loadS3Images(currImageName);
+                loadSingleImage(currImageName);
             }
         }
     });
 }
 
-function loadS3Images(currImageName) {
-    s3Client.getSignedUrl('getObject', {Bucket: imageBucketName, Key: currImageName}, function(err, url) {
+function loadSingleImage(currImageName) {
+    s3.getSignedUrl('getObject', {Bucket: imageBucketName, Key: currImageName}, function(err, url) {
         if (err) {
             console.error(err);
         } else {
---
+++ storyline.php
@@ -0,0 +1,18 @@
+<?php
+	$db = new PDO("mysql:host=keycynthia-timeline.cupo2yigmrng.us-east-1.rds.amazonaws.com;dbname=keycynthia;charset=utf8mb4", "kuangyou", "461298cowboy");
+	$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+	$query ="SELECT date, story, type
+			 FROM timeline
+			 ORDER BY date ASC;";
+
+	$rows = $db->query($query);
+	$json = array();
+
+	foreach($rows as $row) {
+		$json[] = $row;
+	}
+
+	header('Content-Type: application/json');
+	print json_encode($json, JSON_UNESCAPED_UNICODE);
+?>
